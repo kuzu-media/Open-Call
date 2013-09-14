1f43f52b89b017127048f179e365016298aa6bfc
Rachel Higley <me@rachelhigley.com>
2013-09-14 11:31:54 -0400

    Created funtionality for the back button

--- app/core/partials.js
+++ app/core/partials.js
@@ -6,3 +6,19 @@ OpenCall.directive("navbar",function()
 		templateUrl: "app/views/partials/navbar.html"
 	};
 });
+
+OpenCall.directive('backButton', function(){
+	return {
+		restrict: 'A',
+
+		link: function(scope, element) {
+
+			element.bind('click', function () {
+				history.back();
+				scope.$apply();
+			});
+
+
+		}
+	};
+});
