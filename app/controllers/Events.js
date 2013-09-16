54d0f4a541b38f76cd0b25e4136704b4f9c6dbf6
Rachel Higley <me@rachelhigley.com>
2013-09-16 12:38:38 -0400

    Create an event [Delivers #56795938]

--- app/controllers/Events.js
+++ app/controllers/Events.js
@@ -1,6 +1,25 @@
 /* global OpenCall */
-OpenCall.controller("Events",[function()
-{
+/*jshint devel:true */
 
+OpenCall.controller("Events",['angularFireCollectionExtended','FIREBASE_URL','$scope','$filter',function(angularFireCollectionExtended,FIREBASE_URL,$scope,$filter)
+{
+	angularFireCollectionExtended(FIREBASE_URL+"categories/sub").then(function(subcategories)
+	{
+		$scope.subcategories = subcategories;
+	});
+	angularFireCollectionExtended(FIREBASE_URL+"events").then(function(events)
+	{
+		$scope.events = events;
+	});
+	$scope.event = {};
+
+	$scope.event.date = $filter('date')(new Date(), "MM/dd/yy");
+	$scope.event.time = $filter('date')(new Date(), "hh:mm a");
+	console.log('$scope.event.time',$scope.event.time);
+
+	$scope.create = function()
+	{
+		$scope.events.add($scope.event);
+	};
 }
 ]);
